8736e7e75da151c674583af6389c5088893f98d0
Dika Rizki <dikaesmidorikawa55@gmail.com>
2024-03-06 21:35:06 +0700

    final

--- main.js
+++ main.js
@@ -13,19 +13,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-cube.rotation.x = 0.5;
-// scene.add(cube);
-
 const earthgeo = new THREE.SphereGeometry(1, 100, 100);
 const textureLoader = new THREE.TextureLoader();
 const earthmat = new THREE.MeshStandardMaterial({
   map: textureLoader.load("earthmap1k.jpg"),
 });
 const earth = new THREE.Mesh(earthgeo, earthmat);
-// scene.add(earth);
 
 const light = new THREE.PointLight(0xffffff, 3, 1000);
 light.position.set(2, 0, 3);
@@ -47,16 +40,77 @@ function placeObjectOnPlanet(object, lat, lon, radius) {
 
 const pinsgeo = new THREE.SphereGeometry(0.005, 10, 10);
 const pinsmat = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const pinsneth = new THREE.Mesh(pinsgeo, pinsmat);
-placeObjectOnPlanet(pinsneth, 52.370216, 4.895168, 1);
-
-const pinsbel = new THREE.Mesh(pinsgeo, pinsmat);
-placeObjectOnPlanet(pinsbel, 51.260197, 4.402771, 1);
 
 const object = new THREE.Object3D();
 object.add(earth);
-object.add(pinsneth);
-object.add(pinsbel);
+
+function pinscountry(lat, lon) {
+  const pin = new THREE.Mesh(pinsgeo, pinsmat);
+  placeObjectOnPlanet(pin, lat, lon, 1);
+  return pin;
+}
+
+const country = [
+  {
+    country: "netherlands",
+    lat: 52.370216,
+    lon: 4.895168,
+  },
+  {
+    country: "belgium",
+    lat: 51.260197,
+    lon: 4.402771,
+  },
+  {
+    country: "germany",
+    lat: 52.520008,
+    lon: 13.404954,
+  },
+  {
+    country: "austria",
+    lat: 48.210033,
+    lon: 16.363449,
+  },
+  {
+    country: "sweden",
+    lat: 59.334591,
+    lon: 18.06324,
+  },
+  {
+    country: "finland",
+    lat: 60.192059,
+    lon: 24.945831,
+  },
+  {
+    country: "norway",
+    lat: 62,
+    lon: 10,
+  },
+  {
+    country: "denmark",
+    lat: 55.676098,
+    lon: 12.568337,
+  },
+  {
+    country: "uk",
+    lat: 51.509865,
+    lon: -0.118092,
+  },
+];
+
+const pin = [];
+
+country.forEach((cou) => {
+  pinscountry(cou.lat, cou.lon);
+  pin.push(pinscountry(cou.lat, cou.lon));
+});
+
+pin.forEach((pintag) => {
+  object.add(pintag);
+});
+
+console.log(pin);
+
 // object.rotation.y = 1.0;
 scene.add(object);
 
@@ -66,9 +120,6 @@ camera.lookAt(0, 0, 0);
 function animate() {
   requestAnimationFrame(animate);
   object.rotation.y -= 0.005;
-  //   cube.rotation.x += 0.01;
-  //   cube.rotation.y += 0.01;
-  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
